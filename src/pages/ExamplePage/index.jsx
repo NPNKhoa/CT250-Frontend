760cdaac8b7e669450c40a8b06ee2968f08c5a1b
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-08-12 13:05:31 +0700

    initial commit

---
+++ src/pages/ExamplePage/index.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import PropTypes from 'prop-types';
+
+const ExamplePage = ({ ...props }) => {
+  return (
+    <div {...props}>
+      <h1 className='text-blue-500'>Example Page - Test Tailwindcss</h1>
+    </div>
+  );
+};
+
+ExamplePage.propTypes = {};
+
+export default ExamplePage;
