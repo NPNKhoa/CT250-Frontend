87d12f1b9f22d9146d34cba991b3e8e0c416b1c1
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-08-25 15:35:05 +0700

    fix

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -7,6 +7,9 @@ import CheckWarranty from '@pages/CheckWarranty';
 import ContactPage from '@pages/ContactPage';
 import ProductDetail from '@pages/ProductDetail';
 
+import Products from '@pages/Products';
+import CartPage from '@pages/CartPage';
+
 const routes = [
   {
     id: 'home',
