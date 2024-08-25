d0926b2dc874d2571ea06cc14a3a227089391f2b
Duong Hoai Bao <dhbao2505@gmail.com>
2024-08-26 02:03:45 +0700

    add products page

---
+++ src/components/Filter.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+
+const Filter = () => {
+  const [selectedPrice, setSelectedPrice] = useState([]);
+  const [selectedBrand, setSelectedBrand] = useState([]);
+  const [branch, setBranch] = useState('');
+
+  const handlePriceChange = price => {
+    setSelectedPrice(prev => {
+      if (prev.includes(price)) {
+        return prev.filter(p => p !== price);
+      } else {
+        return [...prev, price];
+      }
+    });
+  };
+
+  const handleBrandChange = brand => {
+    setSelectedBrand(prev => {
+      if (prev.includes(brand)) {
+        return prev.filter(b => b !== brand);
+      } else {
+        return [...prev, brand];
+      }
+    });
+  };
+
+  return (
+    <div className='w-full p-6 bg-gray-50 shadow-lg rounded-lg'>
+  <h3 className='text-lg font-bold mb-6'>Chọn Mức Giá</h3>
+  <div className='space-y-4 mb-6'>
+    {[
+      { label: 'Giá dưới 500.000đ', value: 'under-500k' },
+      { label: '500.000đ - 1 triệu', value: '500k-1m' },
+      { label: '1 - 2 triệu', value: '1m-2m' },
+      { label: '2 - 3 triệu', value: '2m-3m' },
+      { label: 'Giá trên 3 triệu', value: 'above-3m' },
+    ].map((price) => (
+      <label key={price.value} className='block text-sm text-gray-700'>
+        <input
+          type='checkbox'
+          className='mr-3 transform scale-125'
+          value={price.value}
+          onChange={() => handlePriceChange(price.value)}
+        />
+        {price.label}
+      </label>
+    ))}
+  </div>
+
+  <h3 className='text-lg font-bold mb-6'>Thương Hiệu</h3>
+  <div className='space-y-4'>
+    {[
+      { label: 'Yonex', value: 'yonex' },
+      { label: 'Lining', value: 'lining' },
+      { label: 'Victor', value: 'victor' },
+      { label: 'Apacs', value: 'apacs' },
+      { label: 'Kawasaki', value: 'kawasaki' },
+    ].map((brand) => (
+      <label key={brand.value} className='block text-sm text-gray-700'>
+        <input
+          type='checkbox'
+          className='mr-3 transform scale-125'
+          value={brand.value}
+          onChange={() => handleBrandChange(brand.value)}
+        />
+        {brand.label}
+      </label>
+    ))}
+  </div>
+</div>
+
+  );
+};
+
+export default Filter;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,88 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import brandService from '@services/brand.service';
+import productTypeService from '@services/productType.service';
+import productService from '@services/product.service';
+
+import ProductItem from '@components/ProductItem';
+import Filter from '@components/Filter';
+import BreadcrumbsComponent from '@components/Breadcrumb';
+
 const Products = () => {
+  const capitalizeFirstLetter = (str) => { // In hoa chữ cái đầu tiên
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+  const normalizeString = str => { // Chuyển chuỗi thành chuỗi không dấu
+    return str
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  };
+
+  const { id } = useParams();
+  const [productTypes, setProductTypes] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const parts = id.split('-');
+  const type = parts.slice(0, -1).join('-');
+  const brand = capitalizeFirstLetter(parts[parts.length - 1]);
+  
+
+  useEffect(() => {
+    const fetchProductTypes = async () => {
+      try {
+        setProducts([]);
+  
+        const responseType = await productTypeService.getAll();
+        setProductTypes(responseType.data);
+        const responsebrand = await brandService.getAll();
+        setBrands(responsebrand.data);
+  
+        if (id === '') {
+          const responseProduct = await productService.getAll();
+          setProducts(responseProduct.data);
+        } else {
+          const responseProduct = await productService.getByName(brand, 1, 10);
+          setProducts(responseProduct.data);
+        }
+      } catch (error) {
+        console.error('Error fetching product types:', error);
+      }
+    };
+    fetchProductTypes();
+  }, [id, brand]);
+
+  console.log(products);
+
+  const breadcrumbs = [
+    { label: 'Trang chủ', href: '/' },
+    { label: 'Vợt cầu lông', href: '/products/' },
+    { label: `Vợt cầu lông ${brand}` },
+  ];
+
   return (
-    <div>
-      <h1>Products</h1>
+    <>
+    <BreadcrumbsComponent breadcrumbs={breadcrumbs} />
+    <div className="flex p-4">
+      <div className="w-1/5 m-1">
+        <Filter />
+      </div>
+      <div className="w-4/5 grid grid-cols-4 gap-1 ml-2">
+        {Array.isArray(products) && products.map((product, index) => (
+          <ProductItem
+            key={index}
+            imageUrl={product.productImagePath[0]}
+            name={product.productName}
+            price={product.price}
+            productLink={`products/detail/${product._id}`}
+          />
+        ))}
+      </div>
     </div>
+    </>
   );
 };
 
