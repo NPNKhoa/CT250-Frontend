dace68e9ca407523303af6e5ceb52599b0e6b302
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-09-09 15:51:47 +0700

    fix: fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,179 +1,139 @@
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import XIcon from '@mui/icons-material/X';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import MailIcon from '@mui/icons-material/Mail';
+import PhoneIcon from '@mui/icons-material/Phone';
 import { Link } from 'react-router-dom';
-
 import FooterImg from '@assets/BoCongThuong.png';
 
 const Footer = () => {
+  const infoList = [
+    {
+      title: 'Thông tin chung',
+      content: [
+        'VNB Sports là hệ thống cửa hàng thể thao chuyên cung cấp sỉ và lẻ các mặt hàng dụng cụ thể thao từ phong trào tới chuyên nghiệp.',
+        'Với sứ mệnh: "VNB cam kết mang đến những sản phẩm, dịch vụ chất lượng tốt nhất phục vụ cho người chơi thể thao để nâng cao sức khỏe của chính mình."',
+        'Tầm nhìn: "Trở thành nhà phân phối và sản xuất thể thao lớn nhất Việt Nam."',
+      ],
+    },
+    {
+      title: 'Chính sách',
+      links: [
+        { label: 'Chính sách đổi trả, hoàn tiền', link: '#' },
+        { label: 'Chính sách bảo hành', link: '#' },
+        { label: 'Chính sách xử lý khiếu nại', link: '#' },
+        { label: 'Chính sách vận chuyển', link: '#' },
+        { label: 'Điều khoản sử dụng', link: '#' },
+        { label: 'Chính Sách Bảo Mật Thông Tin', link: '#' },
+      ],
+    },
+    {
+      title: 'Hướng dẫn',
+      links: [
+        { label: 'Hướng dẫn thanh toán', link: '#' },
+        { label: 'Kiểm tra bảo hành', link: '#' },
+        { label: 'Kiểm tra đơn hàng', link: '#' },
+        { label: 'HƯỚNG DẪN MUA HÀNG', link: '#' },
+      ],
+    },
+    {
+      title: 'Thông tin liên hệ',
+      links: [
+        {
+          label: '8:00 - 22:00',
+          link: '#',
+          icon: <AccessTimeIcon className='text-white mr-2' />,
+        },
+        {
+          label: '3/2, Xuân Khánh, Ninh Kiều, Cần Thơ',
+          link: '#',
+          icon: <LocationOnIcon className='text-white mr-2' />,
+        },
+        {
+          label: 'minhtu15112k3gmail.com',
+          link: '#',
+          icon: <MailIcon className='text-white mr-2' />,
+        },
+        {
+          label: '0977508430 | 0792677415',
+          link: '#',
+          icon: <PhoneIcon className='text-white mr-2' />,
+        },
+      ],
+    },
+  ];
+
+  const socialIcons = [
+    {
+      icon: <FacebookIcon className='text-white hover:text-primary' />,
+      link: '#',
+    },
+    {
+      icon: <InstagramIcon className='text-white hover:text-primary' />,
+      link: '#',
+    },
+    { icon: <XIcon className='text-white hover:text-primary' />, link: '#' },
+  ];
+
   return (
-    <div className=' '>
-      <div className='grid grid-cols-4 bg-black gap-8 px-28'>
-        <div className=''>
-          <h2 className='text-white uppercase text-base font-bold py-2'>
-            Thông tin chung
-          </h2>
-          <p className='text-white text-justify text-sm pb-2'>
-            <span className='text-primary font-semibold '>VNB Sports</span> là
-            hệ thống cửa hàng cầu lông với hơn 50 chi nhánh trên toàn quốc, cung
-            cấp sỉ và lẻ các mặt hàng dụng cụ cầu lông từ phong trào tới chuyên
-            nghiệp
-          </p>
-          <p className='text-white  text-justify text-sm pb-2 italic '>
-            <span className='text-primary font-semibold  not-italic'>
-              Với sứ mệnh:{' '}
-            </span>
-            &quot;VNB cam kết mang đến những sản phẩm, dịch vụ chất lượng tốt
-            nhất phục vụ cho người chơi thể thao để nâng cao sức khỏe của chính
-            mình.&quot;
-          </p>
-          <p className='text-white italic text-justify text-sm pb-2'>
-            <span className='text-primary not-italic'>Tầm nhìn: </span>{' '}
-            &quot;Trở thành nhà phân phối và sản xuất thể thao lớn nhất Việt
-            Nam&quot;
-          </p>
-        </div>
+    <div className='bg-black/90'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-6 md:px-12 lg:px-28 py-8'>
+        {infoList.map((section, index) => (
+          <div key={index}>
+            <h2 className='text-white uppercase text-base font-bold py-2'>
+              {section.title}
+            </h2>
+            {section.content && (
+              <div>
+                {section.content.map((paragraph, i) => (
+                  <p key={i} className='text-white text-sm pb-2 text-justify'>
+                    {paragraph}
+                  </p>
+                ))}
+              </div>
+            )}
+            {section.links && (
+              <ul>
+                {section.links.map((link, i) => (
+                  <li
+                    key={i}
+                    className='text-white pb-2 text-sm flex items-center hover:text-primary'
+                  >
+                    {link.icon && link.icon}
+                    <Link to={link.link}>{link.label}</Link>
+                  </li>
+                ))}
+              </ul>
+            )}
 
-        <div className=''>
-          <h2 className='text-white uppercase text-base font-bold py-2'>
-            Thông tin liên hệ
-          </h2>
-          <div className='pb-5'>
-            <p className=' text-white text-sm'>
-              <span className='uppercase'>Hệ thống cửa hàng: </span>{' '}
-              <span className='text-primary'>
-                5 shop Premium và 63 cửa hàng
-              </span>{' '}
-              trên toàn quốc
-            </p>
-            <Link to='/'>
-              <p className='text-primary font-bold mt-2'>
-                Xem tất cả các cửa hàng VNB
-              </p>
-            </Link>
+            {section.title === 'Hướng dẫn' && (
+              <img src={FooterImg} alt='Bộ Công Thương' className='mt-4 h-18' />
+            )}
+            {section.title === 'Thông tin liên hệ' && (
+              <div className=''>
+                <h2 className='text-white uppercase text-sm font-bold py-2'>
+                  Kết nối với chúng tôi
+                </h2>
+                <div className=' space-x-4 mt-1'>
+                  {socialIcons.map((icon, index) => (
+                    <a key={index} href={icon.link} className='text-white '>
+                      {icon.icon}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )}
           </div>
-
-          <div className=''>
-            <ul className=''>
-              <li className='text-white pb-2'>
-                Hotline:{' '}
-                <span className='text-primary'> 0977508430 | 0792677415</span>
-              </li>
-              <li className='text-white pb-2'>
-                Email:{' '}
-                <span className='text-primary'> minhtu15112k3gmail.com</span>
-              </li>
-              <li className='text-white pb-2'>
-                Hợp tác kinh doanh:{' '}
-                <span className='text-primary'> 012345789 (Mr. Tu)</span>
-              </li>
-              <li className='text-white pb-2'>
-                Hotline bán sỉ:{' '}
-                <span className='text-primary'> 0977508430</span>
-              </li>
-              <li className='text-white pb-2'>
-                Nhượng quyền thương hiệu:{' '}
-                <span className='text-primary'>012345789 (Mr. Tu)</span>
-              </li>
-              <li className='text-white pb-2'>
-                Than phiền dịch vụ:{' '}
-                <span className='text-primary'>012345789 (Mr. Tu)</span>
-              </li>
-            </ul>
-          </div>
-
-          <div className=''>
-            <ul className='flex gap-4 py-3 mb-2'>
-              <li className=''>
-                <FacebookIcon className='text-white' />
-              </li>
-              <li>
-                <InstagramIcon className='text-white' />
-              </li>
-              <li>
-                <XIcon className='text-white' />
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className=''>
-          <h2 className='text-white uppercase text-base font-bold py-2'>
-            Chính sách
-          </h2>
-
-          <div className=''>
-            <ul>
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Chính sách đổi trả,hoàn tiền</Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Chính sách bảo hành</Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Chính sách xử lý khiếu nại</Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Chính sách vận chuyển</Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Điều khoản sử dụng</Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Chính Sách Bảo Mật Thông Tin</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className=''>
-          <h2 className='text-white uppercase text-base font-bold py-2'>
-            Hướng dẫn
-          </h2>
-          <div className=''>
-            <ul>
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>
-                  Tìm hiểu công việc tập huấn tennis trước khi tổ chức giải đấu
-                </Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>
-                  Bảng size Lining - Cách chọn size quần áo lining, giày cầu
-                  lông Lining
-                </Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Hướng dẫn cách tập tennis cho người mới chơi</Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>
-                  Hướng dẫn cách chọn vợt cầu lông cho người mới chơi
-                </Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Hướng dẫn thanh toán</Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Kiểm tra bảo hành</Link>
-              </li>
-              <li className='text-white pb-2 text-sm hover:text-primary'>
-                <Link to='/'>Kiểm tra đơn hàng</Link>
-              </li>{' '}
-              <li className='text-white pb-2 text-sm hover:text-primary uppercase'>
-                <Link to='/'>HƯỚNG DẪN MUA HÀNG</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+        ))}
       </div>
 
-      <div className='bg-primary '>
-        <div className='py-5 text-white flex flex-col items-center text-center'>
-          <p>© Copyright 2024 By BaKaTu</p>
-          <p>GĐ/Sở hữu website: Nguyễn Minh Tứ</p>
-          <img src={FooterImg} alt='' />
-        </div>
+      <div className='py-3 bg-black text-center'>
+        <p className='text-white text-sm'>
+          © Copyright {new Date().getFullYear()} By BaKaTu | GĐ/Sở hữu website:
+          Nguyễn Minh Tứ
+        </p>
       </div>
     </div>
   );
