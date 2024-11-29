1b026f1602d151389a2d15b11cd105b9992bf09d
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-11-30 02:51:50 +0700

    fix UI homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -184,7 +184,7 @@ const HomePage = () => {
       <section className='py-4'>
         <div className='container mx-auto px-4'>
           <h2 className='text-3xl font-bold text-center hover:text-primary mb-4'>
-            Có thể bạn cũng thích
+            Sản phẩm nổi bật
           </h2>
           <span className='mb-6 bg-primary h-2 rounded flex justify-center w-[50vw] md:w-[30vw] mx-auto'></span>
           <SuggestedProducts />
